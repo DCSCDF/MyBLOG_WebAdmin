22c27c5bdb64fba533c9650c02a377a5c9abf218
JiuLiutop <3209174373@qq.com>
2026-02-02 18:23:37 +0800

    版权

--- src/utils/apiInterceptor.js
+++ src/utils/apiInterceptor.js
@@ -6,7 +6,7 @@
  * See LICENSE for full terms.
  * --------------------------------------------------------------------------------
  * author: "Jiu Liu"
- * author_contact: "QQ:3209174373, GitHub：https://github.com/DCSCDF"
+ * author_contact: "QQ: 3209174373, GitHub: https://github.com/DCSCDF"
  * license: "MIT"
  * license_exception: "Mandatory attribution retention"
  * UpdateTime: 2026/2/2 18:17
